30d9404f57bfa2736e81d497b1c86cf37af73ba4
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2024-07-28 16:18:15 +0200

    server and data added

---
+++ data.js
@@ -0,0 +1,7 @@
+const series = {
+    title: 'Doctor Who',
+    origin: 'UK',
+    year: 'since 1963'
+}
+
+module.exports = series
---
+++ server.js
@@ -0,0 +1,30 @@
+const series = require('./data.js')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+   
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+   
+    const html = `
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>${series.title}</title>
+            </head>
+            <body>
+                <h1>${series.title}</h1>
+                <p>Origin: ${series.origin}</p>
+                <p>Year: ${series.year}</p>
+            </body>
+        </html>
+    `
+
+    res.end(html);
+  });
+  
+  const port = 3000;
+  server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
